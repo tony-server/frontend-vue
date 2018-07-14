3327ea16de25ba7e478f08f133622172bef4e52a
lyman <cheerfultony@gmail.com>
2018-07-14 19:18:00 +0800

    debug

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     // eslint-disable-next-line
     /* eslint-disable */
     'generator-star-spacing': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
     'no-undef': 'off'
