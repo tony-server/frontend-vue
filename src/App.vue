5cb1fb1796c09ddae78326b26cef9f9e440d280e
Lyman <cheerfultony@gmail.com>
2018-07-15 12:46:51 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,18 @@
   }
 </script>
 
-<style>
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+  .size;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
+// import {routers, otherRouter, appRouter} from './router'
+
 // 全加载
 // import Index from '../components/Index'
 // 懒加载
 const Index = () => import('../components/Index')
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+//export const router = new VueRouter(RouterConfig)
+
+export default new VueRouter({
   routes: [{
     path: '/',
     component: Index
